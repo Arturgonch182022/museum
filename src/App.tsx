74772462c12160d6fd15e223cbc00f4ff989f4b7
artur <arturgonch20032018@mail.ru>
2025-01-19 15:44:42 +0300

    Deploy updated build files

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <BurgerMenu />
             <Routes>
                 <Route path="/" element={<HomePage />} />
