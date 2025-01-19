71f2feabd17035eb0696efb0ba7d1282f56ba931
artur <arturgonch20032018@mail.ru>
2025-01-19 21:33:53 +0300

    create project

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 
 function App() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter >
             <BurgerMenu />
             <Routes>
                 <Route path="/" element={<HomePage />} />
