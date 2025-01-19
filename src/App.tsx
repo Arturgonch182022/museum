9f14b2a2873066ffeca8be6c42c0a3a82d601428
artur <arturgonch20032018@mail.ru>
2025-01-19 22:42:41 +0300

    feat: add deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import HomePage from "./pages/HomePage/HomePage.tsx";
 
 function App() {
     return (
-        <BrowserRouter >
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
             <BurgerMenu />
             <Routes>
                 <Route path="/" element={<HomePage />} />
